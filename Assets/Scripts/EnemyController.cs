ce0b86260d2246c95a8ca2bd6f08aad3a9d1ef0b
Brian <143449504+BrianAr17@users.noreply.github.com>
2025-10-01 11:37:48 -0400

    to do later I guess

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,16 +4,25 @@ public class EnemyController : MonoBehaviour
 {
     private Animator animator;
     private ParticleSystem system;
+    private AudioSource audioSource;
     void Start()
     {
         animator = GetComponent<Animator>();
         system = GetComponent<ParticleSystem>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void GotHit()
     {
         animator.ResetTrigger("GotHit");
         animator.SetTrigger("GotHit");
+        system.Play();
+        audioSource.Play();
+
+        if(GameManager.Instance != null)
+        {
+            GameManager.Instance.AddScore(1);
+        }
 
     }
 }
